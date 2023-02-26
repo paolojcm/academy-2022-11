6f29df69c9ee7293d5e821bb06d7a49b1ad09084
paolojcm@gmail.com <paolojcm@gmail.com>
2023-02-26 23:05:12 +0000

    Continuação da aula

--- models/staging/erp_northwind/stg_erp__categorias.sql
+++ models/staging/erp_northwind/stg_erp__categorias.sql
@@ -3,8 +3,7 @@ with
         select 
             cast(category_id as int) as id_categoria
             , cast(category_name as string) as nome_categoria
-            , cast(description as string) as descricao
-            --, picture
+            , cast(description as string) as descricao_categoria
         from {{ source('erp', 'categories')}}
     )
 
