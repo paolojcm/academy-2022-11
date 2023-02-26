bf21c69eb6e51cef379f1246400a227b49e11751
paolojcm@gmail.com <paolojcm@gmail.com>
2023-02-26 23:53:11 +0000

    Atualização

--- models/staging/erp_northwind/stg_erp__transportadoras.sql
+++ models/staging/erp_northwind/stg_erp__transportadoras.sql
@@ -2,8 +2,8 @@ with
     source_shippers as (
         select 
             cast(shipper_id as int) as id_transportadora
-            , cast(company_name as string) as nome_da_transportadora
-            , cast(phone as string) as telefone
+            , cast(company_name as string) as nome_transportadora
+            , cast(phone as string) as telefone_transportadora
         from {{ source('erp', 'shippers')}}
     )
 
