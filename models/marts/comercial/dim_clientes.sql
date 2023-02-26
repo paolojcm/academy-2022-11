8b805ff53eeedacff2a3716ffb4dd82ca147f683
paolojcm@gmail.com <paolojcm@gmail.com>
2023-02-26 23:43:01 +0000

    Atualização

---
+++ models/marts/comercial/dim_clientes.sql
@@ -0,0 +1,15 @@
+with
+    clientes as (
+        select *            
+        from {{ ref('stg_erp__clientes')}}
+    )
+
+    , transformacoes as (
+        select
+            row_number() over (order by id_cliente) as sk_cliente
+            , *
+        from clientes
+    )
+
+    select *
+    from transformacoes
