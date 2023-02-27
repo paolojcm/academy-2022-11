b8e5e6e8c147b7b186cd185fba187728349f227a
paolojcm@gmail.com <paolojcm@gmail.com>
2023-02-27 07:59:25 +0000

    Atualizações

--- models/marts/comercial/intermediates/int_vendas__pedido_itens.sql
+++ models/marts/comercial/intermediates/int_vendas__pedido_itens.sql
@@ -29,7 +29,26 @@ with
     )
 
     , joined as (
-        select *    
+        select 
+            pedidos.id_pedido
+            , pedidos.id_funcionario
+            , pedidos.id_cliente
+            , pedidos.id_transportadora            
+            , pedido_itens.id_produto           
+            , pedido_itens.desconto
+            , pedido_itens.preco_da_unidade
+            , pedido_itens.quantidade             
+            , pedidos.frete
+            , pedidos.data_do_pedido
+            , pedidos.data_do_envio
+            , pedidos.data_requerida  
+            , pedidos.destinatario
+            , pedidos.endereco_destinatario
+            , pedidos.cep_destinatario
+            , pedidos.cidade_destinatario
+            , pedidos.regiao_destinatario
+            , pedidos.pais_destinatario
+         
         from pedidos
         left join pedido_itens on pedidos.id_pedido = pedido_itens.id_pedido
     )
