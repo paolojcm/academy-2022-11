07c062ccdadccb546dc8f90873ca04b4d0c7f4fe
paolojcm@gmail.com <paolojcm@gmail.com>
2023-02-27 07:16:47 +0000

    Atualização

--- models/marts/comercial/dim_funcionarios.sql
+++ models/marts/comercial/dim_funcionarios.sql
@@ -4,11 +4,34 @@ with
         from {{ ref('stg_erp__funcionarios')}}
     )
 
+    , self_joined as (
+        select
+            funcionarios.id_funcionario				
+            , funcionarios.id_gerente	
+            , funcionarios.sobrenome_do_funcionario	
+            , funcionarios.nome_do_funcionario				
+            , funcionarios.nome_completo_funcionario		
+            , funcionarios.cargo				
+            , funcionarios.titulo_de_cortesia				
+            , funcionarios.data_de_nascimento				
+            , funcionarios.data_da_contratacao		
+            , gerentes.nome_completo_funcionario as gerente		
+            , funcionarios.as endereco				
+            , funcionarios.as cidade					
+            , funcionarios.as regiao					
+            , funcionarios.as cep	
+            , funcionarios.as pais					
+            , funcionarios.as telefone				
+            , funcionarios.observacoes as observacoes         
+        from funcionarios
+        left join funcionarios as gerentes on funcionarios.id_funcionario = gerentes.id_gerente
+    )
+
     , transformacoes as (
         select
             row_number() over (order by id_funcionario) as sk_funcionario
             , *
-        from funcionarios
+        from self_joined
     )
 
     select *
