199b302bb8a96c1132a154355e896ee681bfbbda
paolojcm@gmail.com <paolojcm@gmail.com>
2023-02-27 07:42:48 +0000

    Atualizações

---
+++ models/marts/comercial/fct_vendas.sql
@@ -0,0 +1,20 @@
+with
+    clientes as (
+        select *            
+        from {{ ref('dim_clientes')}}
+    )
+
+    , funcionarios as (
+        select *            
+        from {{ ref('dim_funcionarios')}}
+    )
+
+    , produtos as (
+        select *            
+        from {{ ref('dim_produtos')}}
+    )
+
+    , transportadoras as (
+        select *            
+        from {{ ref('dim_transportadoras')}}
+    )
